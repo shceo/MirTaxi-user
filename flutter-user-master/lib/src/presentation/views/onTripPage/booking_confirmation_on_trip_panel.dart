3029257a09747362ecb17640712a7d9a01350192
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2026-02-14 09:16:39 +0500

    в

--- flutter-user-master/lib/src/presentation/views/onTripPage/booking_confirmation_on_trip_panel.dart
+++ flutter-user-master/lib/src/presentation/views/onTripPage/booking_confirmation_on_trip_panel.dart
@@ -532,6 +532,7 @@ extension _BookingConfirmationOnTripPanel on _BookingConfirmationState {
     return Positioned(
       left: 0,
       right: 0,
+      top: 0,
       bottom: 0,
       child: SafeArea(
         top: false,
